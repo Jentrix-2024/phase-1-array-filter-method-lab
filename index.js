b46f24700af043ca4ec0202b09bcffa7c1b12ab7
Jentrix <jentrix.ilusa@student.moringaschool.com>
2023-09-27 15:31:48 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+function findMatching(drivers,name) {
+   return drivers.filter(
+    (possibleMatch)=>possibleMatch.toLowerCase() === name.toLowerCase()
+   )
+    }
+  function fuzzyMatch(drivers,test){
+    return drivers.filter(
+(possibleMatch)=>possibleMatch.toLowerCase().indexOf(test.toLowerCase())===0);
+    
+  }
+  function matchName(drivers,searchName){
+    return drivers.filter((record)=>record.name===searchName);
+  }
+  
